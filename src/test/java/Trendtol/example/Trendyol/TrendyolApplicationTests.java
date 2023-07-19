8f3c9c0403f4f2cd71191fa633e37d2c9a05d789
aayzfatih <fatihayaz076@gmail.com>
2023-07-19 14:50:02 +0300

    packaging has been done

---
+++ src/test/java/Trendtol/example/Trendyol/TrendyolApplicationTests.java
@@ -0,0 +1,13 @@
+package Trendtol.example.Trendyol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendyolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
